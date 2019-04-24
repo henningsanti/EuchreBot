6994fdca3171c1e7f9335971b56f2a6c1cc70947
Santiago Henning <shenningmanrique@gmail.com>
2019-04-24 19:47:34 -0400

    fixed bug that occurred when deciding legal moves

--- RandomDecider.py
+++ RandomDecider.py
@@ -38,9 +38,10 @@ class RandomDecider(GUIPlayer):
         time.sleep(SLEEPTIME)
 
         my_list = []
-        lead_suit = None if len(field) == 0 else field[0][1].suit
+        lead_card = None if len(field) == 0 else field[0][1]
+
         for card in self.hand:
-            if validate_play_card(lead_suit= lead_suit, card=card, trump=self.game_state.trump, hand=self.hand):
+            if validate_play_card(lead_suit=lead_suit, card=card, trump=self.game_state.trump, hand=self.hand):
                 my_list.append(card)
 
         print('-----TEST-------')
@@ -48,7 +49,7 @@ class RandomDecider(GUIPlayer):
         print('Lead suit: ', lead_suit)
         print('Hand: ', self.hand)
         print(my_list)
-        
+
         choice = random.randint(0,len(my_list)-1)
         return my_list[choice]
 
@@ -58,6 +59,6 @@ class RandomDecider(GUIPlayer):
         time.sleep(SLEEPTIME)
 
         choice = random.randint(0,4)
-        
+
 
         return self.hand[choice]
